e8f9cd3d4b4366505f719538bf598ed0de55300e
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-01-20 19:24:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,17 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     command.init()
     
+    if(configs.autostatus) {
+        
+        setInterval(function () {
+            let status = configs.statuses[Math.floor(Math.random() * configs.statuses.length)]
+            client.user.setActivity(status, {
+                type: 'WATCHING'
+            });
+        }, 6000)
+
+    }
+
 });
 
 client.on('message', message => {
